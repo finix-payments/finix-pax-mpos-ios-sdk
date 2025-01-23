f0020cc433d3ee99e4c214fd3a767c9dc0d95baf
Tom Nguyen <tom.nguyen@finix.com>
2025-01-22 16:57:43 -0800

    feat: initial package release

--- Package.swift
+++ Package.swift
@@ -1,21 +1,27 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PaxMposSDK",
+    platforms: [
+        .iOS("17.6")
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PaxMposSDK",
-            targets: ["PaxMposSDK"]),
+            targets: ["PaxMposSDK"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "PaxMposSDK"),
+        .binaryTarget(
+            name: "PaxMposSDK",
+            path: "./Sources/PaxMposSDK.xcframework"
+        ),
         .testTarget(
             name: "PaxMposSDKTests",
             dependencies: ["PaxMposSDK"]
