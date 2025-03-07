53b8c2a98ebb06b0c4797b41c55c91ccf1f27370
XCode Cloud CI <tom.nguyen@finix.com>
2025-03-07 11:33:40 -0800

    Update PaxMposSDK.xcframework to version 1.3.0

--- Sources/PaxMposSDK.xcframework/ios-arm64/PaxMposSDK.framework/Headers/ci_post_xcodebuild.sh
+++ Sources/PaxMposSDK.xcframework/ios-arm64/PaxMposSDK.framework/Headers/ci_post_xcodebuild.sh
@@ -8,42 +8,46 @@
 
 set -e  # Exit immediately if any command fails
 
-# Only execute if archiving
-if [ "$CI_XCODEBUILD_ACTION" != "archive" ]; then
-  echo "Action is '$CI_XCODEBUILD_ACTION', not 'archive'"
-  echo "Skipping post-archive process"
+# Only execute if archiving using XCode Cloud
+if ! [ "$CI_XCODE_CLOUD" == "true" && "$CI_XCODEBUILD_ACTION" == "archive" ]; then
+  echo "Skipping the process, isXCodeCloud=$CI_XCODE_CLOUD, xcodebuildAction=$CI_XCODEBUILD_ACTION"
   exit 0
 fi
 
-echo "Starting post-archive process for SDK..."
+echo "Starting post-xcodebuild process for SDK..."
 
-# Check if the archive was successful
-if [ ! -d "$CI_ARCHIVE_PATH" ]; then
-  echo "Archive not found at $CI_ARCHIVE_PATH"
+# Check if the archives were successful
+if ! [ -d "$CI_ARCHIVE_PATH" && -d "$SIMULATOR_ARCHIVE_PATH" ]; then
+  echo "Archive not found, CI_ARCHIVE_PATH=$CI_ARCHIVE_PATH, SIMULATOR_ARCHIVE_PATH=$SIMULATOR_ARCHIVE_PATH"
   exit 1
 fi
 
 # Create the xcframework
 echo "Creating xcframework..."
-XCFRAMEWORK_TEMP_PATH="$CI_WORKSPACE_PATH/xcframeworks/PaxMposSDK.xcframework"
+XCFRAMEWORK_TEMP_PATH="$CI_WORKSPACE_PATH/xcframeworks/$CI_PRODUCT.xcframework"
 
 xcodebuild -create-xcframework \
-  -archive "$CI_ARCHIVE_PATH" \
-  -framework PaxMposSDK.framework \
-  -output "$XCFRAMEWORK_TEMP_PATH"
+    -archive "$CI_ARCHIVE_PATH" -framework "$CI_PRODUCT.framework" \
+    -archive "$SIMULATOR_ARCHIVE_PATH" -framework "$CI_PRODUCT.framework" \
+    -output "$XCFRAMEWORK_TEMP_PATH"
 
 # Copy headers to the xcframework
 echo "Copying header files to xcframework..."
-cp -R "./Headers/" * "$XCFRAMEWORK_TEMP_PATH/ios-arm64/PaxMposSDK.framework/Headers/"
+cp -R "./Headers/" * "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/Headers/"
+cp -R "./Headers/" * "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/Headers/"
 
-# Remove ci_post_xcodebuild.sh file from the xcframework
-echo "Removing script file..."
-rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-arm64/PaxMposSDK.framework/ci_post_xcodebuild.sh"
+# Remove ci_pre_xcodebuild.sh and ci_post_xcodebuild.sh files from the xcframework
+echo "Removing script files..."
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/ci_pre_xcodebuild.sh"
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/ci_post_xcodebuild.sh"
+
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/ci_pre_xcodebuild.sh"
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/ci_post_xcodebuild.sh"
 
 # Sign the xcframework
 echo "Signing the xcframework..."
 xcrun codesign --force --sign - --timestamp=none "$XCFRAMEWORK_TEMP_PATH"
-echo "Checking signature after signing"
+echo "Checking signature after signing..."
 codesign -dv "$XCFRAMEWORK_TEMP_PATH" &> /dev/null
 
 # Push to PaxMposSDK SPM repo
@@ -54,7 +58,7 @@ echo "Cloning SPM repository..."
 git clone "$SPM_REPO_URL" "$SPM_REPO_TEMP_PATH"
 
 echo "Replacing xcframework in SPM repo..."
-rm -rf "$SPM_REPO_TEMP_PATH/Sources/PaxMposSDK.xcframework"
+rm -rf "$SPM_REPO_TEMP_PATH/Sources/$CI_PRODUCT.xcframework"
 cp -R "$XCFRAMEWORK_TEMP_PATH" "$SPM_REPO_TEMP_PATH/Sources/"
 
 VERSION="$CI_TAG"
@@ -63,7 +67,7 @@ cd "$SPM_REPO_TEMP_PATH"
 git config user.name "XCode Cloud CI"
 git config user.email "tom.nguyen@finix.com"
 git add .
-git commit -m "Update PaxMposSDK.xcframework to version $VERSION"
+git commit -m "Update $CI_PRODUCT.xcframework to version $VERSION"
 git push origin main
 
 echo "Tagging new version..."
---
+++ Sources/PaxMposSDK.xcframework/ios-x86_64-simulator/PaxMposSDK.framework/Headers/ci_post_xcodebuild.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  PaxMposSDK
+#
+#  Created by Tom Nguyen on 3/3/25.
+#
+
+set -e  # Exit immediately if any command fails
+
+# Only execute if archiving using XCode Cloud
+if ! [ "$CI_XCODE_CLOUD" == "true" && "$CI_XCODEBUILD_ACTION" == "archive" ]; then
+  echo "Skipping the process, isXCodeCloud=$CI_XCODE_CLOUD, xcodebuildAction=$CI_XCODEBUILD_ACTION"
+  exit 0
+fi
+
+echo "Starting post-xcodebuild process for SDK..."
+
+# Check if the archives were successful
+if ! [ -d "$CI_ARCHIVE_PATH" && -d "$SIMULATOR_ARCHIVE_PATH" ]; then
+  echo "Archive not found, CI_ARCHIVE_PATH=$CI_ARCHIVE_PATH, SIMULATOR_ARCHIVE_PATH=$SIMULATOR_ARCHIVE_PATH"
+  exit 1
+fi
+
+# Create the xcframework
+echo "Creating xcframework..."
+XCFRAMEWORK_TEMP_PATH="$CI_WORKSPACE_PATH/xcframeworks/$CI_PRODUCT.xcframework"
+
+xcodebuild -create-xcframework \
+    -archive "$CI_ARCHIVE_PATH" -framework "$CI_PRODUCT.framework" \
+    -archive "$SIMULATOR_ARCHIVE_PATH" -framework "$CI_PRODUCT.framework" \
+    -output "$XCFRAMEWORK_TEMP_PATH"
+
+# Copy headers to the xcframework
+echo "Copying header files to xcframework..."
+cp -R "./Headers/" * "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/Headers/"
+cp -R "./Headers/" * "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/Headers/"
+
+# Remove ci_pre_xcodebuild.sh and ci_post_xcodebuild.sh files from the xcframework
+echo "Removing script files..."
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/ci_pre_xcodebuild.sh"
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-arm64/$CI_PRODUCT.framework/ci_post_xcodebuild.sh"
+
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/ci_pre_xcodebuild.sh"
+rm -rf "$XCFRAMEWORK_TEMP_PATH/ios-x86_64-simulator/$CI_PRODUCT.framework/ci_post_xcodebuild.sh"
+
+# Sign the xcframework
+echo "Signing the xcframework..."
+xcrun codesign --force --sign - --timestamp=none "$XCFRAMEWORK_TEMP_PATH"
+echo "Checking signature after signing..."
+codesign -dv "$XCFRAMEWORK_TEMP_PATH" &> /dev/null
+
+# Push to PaxMposSDK SPM repo
+SPM_REPO_URL="https://x-access-token:${GITHUB_TOKEN}@github.com/finix-payments/PaxMposSDK"
+SPM_REPO_TEMP_PATH="$CI_WORKSPACE_PATH/spm_repo"
+
+echo "Cloning SPM repository..."
+git clone "$SPM_REPO_URL" "$SPM_REPO_TEMP_PATH"
+
+echo "Replacing xcframework in SPM repo..."
+rm -rf "$SPM_REPO_TEMP_PATH/Sources/$CI_PRODUCT.xcframework"
+cp -R "$XCFRAMEWORK_TEMP_PATH" "$SPM_REPO_TEMP_PATH/Sources/"
+
+VERSION="$CI_TAG"
+echo "Updating SPM repo with version $VERSION..."
+cd "$SPM_REPO_TEMP_PATH"
+git config user.name "XCode Cloud CI"
+git config user.email "tom.nguyen@finix.com"
+git add .
+git commit -m "Update $CI_PRODUCT.xcframework to version $VERSION"
+git push origin main
+
+echo "Tagging new version..."
+git tag -a "$VERSION" -m "Version $VERSION"
+git push origin "$VERSION"
+
+echo "Deployment complete! Version $VERSION is now available."
+
+exit 0
