cf7fbb8efe54b3a1850bab113b8fa3c1bfbdfb8f
XCode Cloud CI <tom.nguyen@finix.com>
2025-09-24 11:20:04 -0700

    Update PaxMposSDK.xcframework at 76f85bcc8ea6e9dc930185b11712b6a4740f296f

---
+++ Sources/PaxMposSDK.xcframework/ios-arm64/PaxMposSDK.framework/Headers/PaxMposSDK.h
@@ -0,0 +1,19 @@
+//
+//  PaxMposSDK.h
+//  PaxMposSDK
+//
+//  Created by Tom Nguyen on 1/22/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaxMposSDK.
+FOUNDATION_EXPORT double PaxMposSDKVersionNumber;
+
+//! Project version string for PaxMposSDK.
+FOUNDATION_EXPORT const unsigned char PaxMposSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaxMposSDK/PublicHeader.h>
+
+
+#import "PaxEasyLinkController.h"
